eb702be4a908966f89e218b6eb0c379c98e7a978
John <yyoungjune@gmail.com>
2022-06-24 20:49:11 -0700

    temporarily breaking code

--- verbose_umbrella/src/components/Results.js
+++ verbose_umbrella/src/components/Results.js
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react';
 function Results(props) {
     const [level, changeLevel] = useState('loading');
     let { name } = useParams();
-    let link = '/api/getLevel/' + name;
+    let link = '/api/getKey';
     useEffect(() => {
         fetch(link)
             .then(resp => resp.json())
             .then(result => {
                 console.log(result);
-                changeLevel(result.level);
+                console.log(result.key);
             })
             .catch(err => {
                 console.log(err);
