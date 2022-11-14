a6d6cbc7e546c6b58537212a633efcc71177f37f
John <yyoungjune@gmail.com>
2022-11-14 01:20:39 -0800

    added build elsewhere

--- api/app.js
+++ api/app.js
@@ -27,5 +27,5 @@ app.listen(PORT, () => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../verbose_umbrella/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
