0d56681cf4d78bfb1834a351f1bbb95bf9ec8678
John <yyoungjune@gmail.com>
2022-11-19 13:46:14 -0800

    prepared deployment to render

--- api/app.js
+++ api/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { dirname }  from 'path';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
 
 // route imports
 import apiRouter from './routes/playerData.js';
@@ -13,7 +12,6 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.use(express.static(path.resolve(__dirname, '../verbose_umbrella/build')));
-dotenv.config({ path: path.join(__dirname, '.env') });
 const API_KEY = process.env.RIOT_KEY
 
 app.use((req, res, next) => {
@@ -23,4 +21,9 @@ app.use((req, res, next) => {
 app.use('/api', apiRouter);
 app.listen(PORT, () => {
     console.log(`Server listening on the port::${PORT}`);
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../verbose_umbrella/build', 'index.html'));
 });
