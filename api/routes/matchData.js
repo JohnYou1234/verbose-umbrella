1f8e2c12a7fec4d6a5faa5ea1f703969643c4f6a
John <yyoungjune@gmail.com>
2022-12-21 23:19:24 -0800

    rest of commit, finished dropdown

--- api/routes/matchData.js
+++ api/routes/matchData.js
@@ -36,35 +36,43 @@ function getPlayerData(players, id) {
     let mainPlayerTeam = -1;
     let playerData = players.map((data, index) => {
         let player = {};
-        player.victory = data.win;
-        player.championIcon = 'http://ddragon.leagueoflegends.com/cdn/12.22.1/img/champion/' + champions.data[data.championName].image.full;
-        player.name = data.summonerName;
-        player.kills = data.kills;
         player.assists = data.assists;
-        player.deaths = data.deaths;
+        player.champion = data.championName;
+        player.championIcon = 'http://ddragon.leagueoflegends.com/cdn/12.22.1/img/champion/' + champions.data[data.championName].image.full;
         player.creepScore = data.totalMinionsKilled;
+        player.damage = data.totalDamageDealtToChampions;
+        player.deaths = data.deaths;
+        player.goldEarned = data.goldEarned;
+        player.items = [data.item0, data.item1, data.item2, data.item3, data.item4, data.item5, data.item6];
+        player.kills = data.kills;
+        player.name = data.summonerName;
         player.summoner1 = summonerSpells[data.summoner1Id];
         player.summoner2 = summonerSpells[data.summoner2Id];
-        player.items = [data.item0, data.item1, data.item2, data.item3, data.item4, data.item5, data.item6];
         let styles = data.perks.styles;
         for (let i = 0; i < styles.length; i++) {
             if (data.perks.styles[i].description == "primaryStyle") {
-                const keyStone = runes[styles[i].style].slots[0].runes.find(obj => obj.id === styles[i].selections[0].perk);
+                const keyStone = runes[styles[i].style].slots[0].runes.find(obj => obj.id === styles[i].selections[0].perk); 
                 player.primaryIcon = 'https://ddragon.canisback.com/img/' + keyStone.icon;
+                player.primary = styles[i].style;
             } else {
                 player.secondaryIcon = 'https://ddragon.canisback.com/img/' + runes[styles[i].style].icon;
+                player.secondary = styles[i].style;
             }
         }
         if (id === data.puuid) {
             mainPlayer = player;
             mainPlayerTeam = Math.floor(index / 5);
+            mainPlayer['victory'] = data.win;
         }
         return player;
     })
     let data = {};
     data.mainPlayer = mainPlayer;
-    data.allyTeam = playerData.slice(mainPlayerTeam * 5, mainPlayerTeam * 5 + 5); // 0-5 5-10
-    data.enemyTeam = playerData.slice(5 - mainPlayerTeam * 5, 5 - mainPlayerTeam * 5 + 5); // 5-10 0-5
+    data.allyTeam = {}; data.enemyTeam = {};
+    data.allyTeam['victory'] = mainPlayer.victory;
+    data.enemyTeam['victory'] = !mainPlayer.victory;
+    data.allyTeam['members'] = playerData.slice(mainPlayerTeam * 5, mainPlayerTeam * 5 + 5); // 0-5 5-10
+    data.enemyTeam['members'] = playerData.slice(5 - mainPlayerTeam * 5, 5 - mainPlayerTeam * 5 + 5); // 5-10 0-5
     return data;
 }
 
